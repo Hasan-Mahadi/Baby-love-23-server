6f2259306f3b0ecc14d8c1218054b284d4106236
Hasan-Mahadi <hasanmahadihm99@gmail.com>
2023-12-15 22:32:53 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -7,6 +9,39 @@ const categories = require('./data/categories.json');
 const products = require('./data/products.json');
 
 app.use(cors());
+
+
+console.log(process.env.DB_PASS)
+console.log(process.env.DB_USER)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster-0.djotxke.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        await client.connect();
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+        await client.close();
+    }
+}
+run().catch(console.log);
+
+
+
+
+
 
 
 app.get('/', (req, res) => {
@@ -35,7 +70,7 @@ app.get('/products/:id', (req, res) => {
 
 app.get('/categories/:id', (req, res) => {
     const id = parseInt(req.params.id);
-  
+
     if (id === 0) {
         res.send(products)
     }
